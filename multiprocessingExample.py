6da270d3d8cd525b25f651d0f8a68f25fc7185e7
Gopi Krishna <kothagopikrishna18@gmail.com>
2025-02-27 12:29:31 +0530

    added something new

--- multiprocessingExample.py
+++ multiprocessingExample.py
@@ -4,6 +4,8 @@ import time
 def square(x):
     return x * x
 
+#added comment for github
+
 if __name__ == '__main__':
     with multiprocessing.Pool(processes=4) as pool:
         results = pool.map(square, range(11))
